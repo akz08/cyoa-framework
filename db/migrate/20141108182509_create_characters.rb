fb0ffec437245ba2f00511930adc88c0c4809340
Amer Kamil Zainal <amer.kamil@gmail.com>
2014-11-12 19:20:13 +0000

    Add age column to Character

--- db/migrate/20141108182509_create_characters.rb
+++ db/migrate/20141108182509_create_characters.rb
@@ -3,6 +3,7 @@ class CreateCharacters < ActiveRecord::Migration
   	create_table :characters do |t|
   		t.string :char_id
   		t.string :name
+  		t.integer :age
   		t.text :description
   	end
   end
