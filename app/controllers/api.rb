15835f08a77807f6a0c06081e1a98df923728e67
Tharman Anantharajan <tharman@gmail.com>
2015-06-17 22:58:24 +0100

    Corrected characters base route - passes basic tests
    Need to finish specific character route by id, before
    moving onto other routes.

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -16,6 +16,7 @@ module CYOA
 		format :json
 
 		helpers do
+
 			def is_authorised?(fb_user_id, fb_access_token)
 				# # Verify user
 				# response = HTTParty.get("https://graph.facebook.com/me", :query => { :access_token => fb_access_token })
@@ -27,11 +28,13 @@ module CYOA
 				# return false if json['data']['app_id'] != {INSERT fb_app_id HERE}
 				true
 			end
+
 		end
 
 		resource :auth do
+
 			params do
-				requires :fb_user_id, type: Integer, desc: "User's Facebook user id."
+				requires :fb_user_id, type: Integer, desc: "User's unique Facebook user id."
 				requires :fb_access_token, type: String, desc: "Valid Facebook access token."
 
 				optional :first_name, type: String, desc: "User's first name."
@@ -41,35 +44,61 @@ module CYOA
 
 			desc "Authenticate a user by using a Facebook access token"
 			post do
-				error! "401: Access denied.", 401 unless is_authorised?(params['fb_user_id'], params['fb_access_token'])
+				error! "Access denied.", 401 unless is_authorised?(params['fb_user_id'], params['fb_access_token'])
 				begin
-					User.find_by! fb_user_id: params['fb_user_id']
+					User.find_by!(fb_user_id: params['fb_user_id'])
 					status 200
 				rescue ActiveRecord::RecordNotFound
-					user = User.create(
+					user = User.new(
 						:fb_user_id => params['fb_user_id'],
 						:first_name => params['first_name'],
 						:last_name => params['last_name'],
 						:email => params['email']
 					)
-					error! "400: Could not create user.", 400 unless user.valid?
+					error! "Could not create user.", 400 unless user.valid?
+					user.save
 					status 201
 				end
 			end
+
 		end
 
 		resource :characters do
+
 			params do
-				requires :fb_user_id, type: Integer, desc: "User's Facebook user id."
+				requires :fb_user_id, type: Integer, desc: "User's unique Facebook user id."
 				requires :fb_access_token, type: String, desc: "Valid Facebook access token."
 			end
 
-			desc "Return information of all characters available to user"
+			desc "Return information of all characters available to the user"
 			get do
-				error! "401 Access denied.", 401 unless is_authorised?(params['fb_user_id'], params['fb_access_token'])
+				error! "Access denied.", 401 unless is_authorised?(params['fb_user_id'], params['fb_access_token'])
 				status 200
-				Character.joins('LEFT OUTER JOIN user_characters ON user_characters.fb_user_id = ' + params['fb_user_id'].to_s).as_json
+				Character.joins("LEFT OUTER JOIN user_characters ON characters.id = user_characters.character_id").where("user_characters.fb_user_id = ?", params['fb_user_id']).as_json
 			end
+
+			desc "Return information for a single character available to the user"
+			params do
+				requires :character_id, type: Integer, desc: "Unique character id."
+			end
+			route_param :character_id do
+				get do
+					error! "Access denied.", 401 unless is_authorised?(params['fb_user_id'], params['fb_access_token'])
+					begin
+						TODO
+					rescue ActiveRecord::RecordNotFound
+						error! "Character is not available to user.", 403
+					end
+					begin
+						character = Character.find_by!(character_id: params['character_id'])
+						status 200
+						return character
+					rescue ActiveRecord::RecordNotFound
+						error! "Character does not exist.", 400
+					end
+				end
+			end
+
 		end
 
 	end
