5b4a854213c98c18415e0725955464eced3f55f3
Tharman Anantharajan <tharman@gmail.com>
2015-06-26 09:51:10 +0100

    Minimalist revision to APIs
    Revised APIs to contain minimal code needed, before implementing 'Messages' API.
    This API should match the format of the others in part.
    
    APIs make use of a conceptual cascading effect in order to avoid re-using
    params and helpers defined prior within code, and to logically separate routes.

--- app/controllers/scenes.rb
+++ app/controllers/scenes.rb
@@ -6,47 +6,46 @@ require_relative '../helpers/authentication'
 require_relative '../helpers/characters'
 require_relative '../helpers/scenes'
 
+require_relative '../models/character'
+require_relative '../models/message'
 require_relative '../models/scene'
 require_relative '../models/user'
 
 class Scenes < Grape::API
 	format :json
 
+	params do
+		requires :fb_user_id, type: Integer
+		requires :api_key, type: String
+	end
+	after_validation do
+		error!("User could not be authenticated.", 401) unless authenticated?(params[:fb_user_id], params[:api_key])
+	end
 	resource :scenes do
-		helpers do
-			params :auth_params do
-				requires :fb_user_id, type: Integer
-				requires :api_key, type: String
-			end
-		end
-
-		after_validation do
-			error!("User could not be authenticated.", 401) unless authenticated?(params[:fb_user_id], params[:api_key])
-		end
-
-		desc "Return static information on all available scenes, for a single character available to the user"
-		params do
-			use :auth_params
-			requires :character_id, type: Integer
-		end
+		desc "Return static information on all scenes available to the user"
 		get do
-			error!("Character does not exist.", 404) unless character_exists?(params[:character_id])
-			error!("Character is not available to user.", 403) unless character_unlocked?(params[:fb_user_id], params[:character_id])
 			status 200
 			User.find(params[:fb_user_id]).scenes
 		end
 
-		desc "Return static information on a single available scene, for a single character available to the user"
 		params do
-			use :auth_params
 			requires :scene_id, type: Integer
 		end
+		after_validation do
+			error!("Scene does not exist.", 404) unless scene_exists?(params[:scene_id])
+			error!("Scene is not available to user.", 403) unless scene_unlocked?(params[:fb_user_id], params[:scene_id])
+		end
 		route_param :scene_id do
+			desc "Return static information on a single scene available to the user"
 			get do
-				error!("Scene does not exist.", 404) unless scene_exists?(params[:scene_id])
-				error!("Scene is not available to user.", 403) unless scene_unlocked?(params[:fb_user_id], params[:scene_id])
 				status 200
-				Scene.find(params[:scene_id])
+				User.find(params[:fb_user_id]).scenes.find(params[:scene_id])
+			end
+
+			desc "Return static information on all exchanged messages, for a single scene available to the user"
+			get :messages do
+				status 200
+				User.find(params[:fb_user_id]).messages.where(scene_id: params[:scene_id])
 			end
 		end
 	end
